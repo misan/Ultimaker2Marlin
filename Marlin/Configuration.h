029f40fa9366e63d24ad8fd2c32a1bfdc420f01e
Lars Norpchen <lars8mo@gmail.com>
2014-06-06 00:31:19 -0700

    update version number in config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -9,7 +9,7 @@
 // startup. Implementation of an idea by Prof Braino to inform user that any changes made to this
 // build by the user have been successfully uploaded into firmware.
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "Version 14.04.1_E" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Version 14.06.1_E" // Who made the changes.
 // _E version added by Lars Norpchen
 
 
